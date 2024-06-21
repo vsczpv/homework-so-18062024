96e828dea358de95a61a252faa278453f2b6184b
Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>
2024-06-21 16:55:39 -0300

    Comment source/commands.c
    Signed-off-by: Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>

--- source/commands.c
+++ source/commands.c
@@ -28,6 +28,13 @@
  *         Nome não faz juz à função
  */
 
+/*
+ * Função de busca na pasta raíz. Codigo original do professor,
+ * altamente modificado.
+ *
+ * Ela itera sobre todas as bpb->possible_rentries do struct fat_dir* dirs, e
+ * retorna a primeira entrada com nome igual à filename.
+ */
 struct far_dir_searchres find_in_root(struct fat_dir *dirs, char *filename, struct fat_bpb *bpb)
 {
 
@@ -55,6 +62,12 @@ struct far_dir_searchres find_in_root(struct fat_dir *dirs, char *filename, stru
 	return res;
 }
 
+/*
+ * Função de ls provida pelo professor.
+ *
+ * Ela itéra todas as bpb->possible_rentries do diretório raiz
+ * e as lê via read_bytes().
+ */
 struct fat_dir *ls(FILE *fp, struct fat_bpb *bpb)
 {
 
@@ -74,36 +87,91 @@ void mv(FILE *fp, char *source, char* dest, struct fat_bpb *bpb)
 
 	char source_rname[FAT16STR_SIZE_WNULL], dest_rname[FAT16STR_SIZE_WNULL];
 
+	/*
+	 * Aqui converte-se os nomes dos arquivos, que estão por padrão no formato
+	 * C-String, misturado minúsculo/maiusculo, com ponto e delimitado por NULL,
+	 * para o formato usado pelo FAT16, tudo maiusculo, ponto implícito, sem
+	 * delimitador e tamanho fixo de 8/3.
+	 *
+	 * Note que o padding() do professor mesmo assim incluí NULL byte, por isso
+	 * a diferença de tramanho entre FAT16STR_SIZE v. FAT16STR_SIZE_WNULL.
+	 */
 	bool badname = cstr_to_fat16wnull(source, source_rname)
 	            || cstr_to_fat16wnull(dest,   dest_rname);
 
+	/*
+	 * Não foi possivel converter de C-String → FAT16str.
+	 */
 	if (badname)
 	{
 		fprintf(stderr, "Nome de arquivo inválido.\n");
 		exit(EXIT_FAILURE);
 	}
 
+	/*
+	 * Aqui usa-se a função, provida pelo professor, para achar o endereço em
+	 * disco do começo da lista de diretórios (da pasta raíz).
+	 *
+	 * Esta informação esta no boot parameter block, que já foi lida fora desta
+	 * função (no main()).
+	 */
 	uint32_t root_address = bpb_froot_addr(bpb);
+
+	/*
+	 * Aqui calcula-se o tamanho, em bytes, da estrutura que guarda as entradas
+	 * de diretório da pasta raíz.
+	 *
+	 * bpb->possible_rentries é a quantidade de entradas alocadas em disco para
+	 * a pasta raíz (padrão 512 entradas).
+	 */
 	uint32_t root_size    = sizeof (struct fat_dir) * bpb->possible_rentries;
 
+	/*
+	 * Le-se, usando read_bytes() função provida pelo professor, do disco as
+	 * entradas de diretório → struct fat_dir root[root_size]
+	 */
 	struct fat_dir root[root_size];
 
 	if (read_bytes(fp, root_address, &root, root_size) == RB_ERROR)
 		error_at_line(EXIT_FAILURE, EINVAL, __FILE__, __LINE__, "erro ao ler struct fat_dir");
 
+	/*
+	 * Então é usado a função find_in_root() para tentar achar alguma entrada
+	 * com os nomes source_rname e dest_rname. Esta função era originalmente
+	 * provida pelo professor mais foi custumizada às nossas necessidades.
+	 *
+	 * Ela não consegue pesquisar em subdiretórios.
+	 */
 	struct far_dir_searchres dir1 = find_in_root(&root[0], source_rname, bpb);
 	struct far_dir_searchres dir2 = find_in_root(&root[0], dest_rname,   bpb);
 
+	/*
+	 * Como regra de negócio, não se deve substituir um arquivo existente com o
+	 * comando mv. Falha-se o comando caso dest já exista.
+	 */
 	if (dir2.found == true)
 		error(EXIT_FAILURE, 0, "Não permitido substituir arquivo %s via mv.", dest);
 
+	/*
+	 * Obviamente, o arquivo que queremos mover, source, deve já existir.
+	 */
 	if (dir1.found == false)
 		error(EXIT_FAILURE, 0, "Não foi possivel encontrar o arquivo %s.", source);
 
+	/*
+	 * Mover o arquivo dentro do mesmo diretório consiste somente em renomeálo.
+	 * Copia-se o nome de destino à estrutura fat_dir que achamos via find_int_root().
+	 */
 	memcpy(dir1.fdir.name, dest_rname, sizeof(char) * FAT16STR_SIZE);
 
+	/*
+	 * Onde em disco está dir1
+	 */
 	uint32_t source_address = sizeof (struct fat_dir) * dir1.idx + root_address;
 
+	/*
+	 * Por fim, aplica-se dir1 em disco.
+	 */
 	(void) fseek (fp, source_address, SEEK_SET);
 	(void) fwrite
 	(
